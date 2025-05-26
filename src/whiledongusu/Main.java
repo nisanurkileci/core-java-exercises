8dee6a7813adc0edee3d2c327a4cd56afa5699ed
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-05-26 18:04:06 +0300

    JA-9

---
+++ src/whiledongusu/Main.java
@@ -0,0 +1,11 @@
+package whiledongusu;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i >= 1) {
+            System.out.print(i);
+            i--;
+        }
+    }
+}
