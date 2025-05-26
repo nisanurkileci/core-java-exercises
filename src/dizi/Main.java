b61a522977f9a8cba869aa7ad92230353c7bed9d
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-05-26 18:07:14 +0300

    JA-10

---
+++ src/dizi/Main.java
@@ -0,0 +1,11 @@
+package dizi;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] sayilar = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < sayilar.length; i++) {
+            System.out.println("Eleman " + i + ": " + sayilar[i]);
+        }
+    }
+}
