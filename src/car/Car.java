8e12cc3ce675b7ab76dc88c96802402e8d152ffe
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-05-30 17:45:43 +0300

    JA-10 Constructor kullanarak Car nesnesi oluşturuldu

--- src/car/Car.java
+++ src/car/Car.java
@@ -4,5 +4,17 @@ public class Car {
     String marka;
     String model;
     int yil;
-}
 
+
+    public Car(String markaA, String modelA, int yilA) {
+        marka = markaA;
+        model = modelA;
+        yil = yilA;
+    }
+
+    public void arababilgileri() {
+        System.out.println("Marka: " + marka);
+        System.out.println("Model: " + model);
+        System.out.println("Yıl: " + yil);
+    }
+}
