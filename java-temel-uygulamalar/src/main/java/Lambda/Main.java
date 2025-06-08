ff721f624d2559cf90a4a7f700b6066a97080177
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-06-09 00:09:56 +0300

    [JA-6] Lambda Expressions

---
+++ java-temel-uygulamalar/src/main/java/Lambda/Main.java
@@ -0,0 +1,16 @@
+package Lambda;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> sayilar = new ArrayList<>();
+        sayilar.add(10);
+        sayilar.add(20);
+        sayilar.add(30);
+        sayilar.add(40);
+        sayilar.add(50);
+
+        sayilar.forEach(sayi -> System.out.println(sayi));
+    }
+}
