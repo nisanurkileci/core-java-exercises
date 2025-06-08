3eb8b04065c6f918a53961cff3d0b51d95ef99b4
nisanurkileci <nnkileci@ogr.eskisehir.edu.tr>
2025-06-08 23:57:49 +0300

    [JA-4] HashMap

---
+++ java-temel-uygulamalar/src/main/java/hashMap/Main.java
@@ -0,0 +1,18 @@
+package hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> notlar = new HashMap<>();
+
+        notlar.put("Ahmet", 90);
+        notlar.put("Ayşe", 85);
+        notlar.put("Mehmet", 75);
+
+        for (Map.Entry<String, Integer> entry : notlar.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
